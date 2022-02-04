16ede711dded1ade38f1ea129aa2cdd61f860c98
Subin <subin9095@naver.com>
2022-02-04 15:31:14 +0900

    로그인,로그아웃,회원가입 완성

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from pybo import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', include('pybo.urls'))
+    path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'), # '/' 에 해당되는 path
+
 ]
